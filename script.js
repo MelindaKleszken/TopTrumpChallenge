46634165e00d68416fbafe6321d656913f55267c
MelindaKleszken <melindakleszken1021@gmail.com>
2021-01-08 16:24:09 +0000

    added comments for rules in js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 console.log('hello')
+
+const imagePlayer1 = document.getElementsByClassName("image-player1");
+const score = document.getElementsByClassName("score");
+const pokeName = document.getElementById("image-player1");
+const HP = document.getElementsByClassName("hp");
+const attack = document.getElementsByClassName("attack");
+const defense = document.getElementsByClassName("defense");
+const speed = document.getElementsByClassName("speed");
+const weight = document.getElementsByClassName("hp");
+
+
+//need to fetch images from pokemon api/ pokecodex (https://assets.pokemon.com/assets/cms2/img/pokedex/detail/024.png)
+
+
+//need to fetch data from pokemon api for stats and name
+
+
+
+
+//need to create gameloop
+
+//once start game button clicked => start loop
+
+
+//need to create random number for game loop
+const i = Math.floor(Math.random() * 20) + 1; 
+console.log(i);
+
+//need to match images in HTML to API images
+
+//need to match stats in HTML to API stats
+
+//use Eventlistener for cards
+
+//compare choosen stat to computers card
+//flip computer card, once player1 choosed his stat
+
+//add exception for limbo
+
